151e5f1ce868f151e9f131378390eb8d01ae95c2
Ken Perlin <ken.perlin@gmail.com>
2026-03-10 14:34:04 -0400

    Small bug fix to beam.js.

--- js/scenes/beam.js
+++ js/scenes/beam.js
@@ -4,9 +4,9 @@ import { ControllerBeam } from "../render/core/controllerInput.js";
 // INTERSECT CONTROLLER BEAMS WITH A RECTANGLE.
 
 export const init = async model => {
-   const inch = .0254, cw = .01271;
+   const inch = .0254;
    let round = t => ((t+'').charAt(0) == '-' ? '' : ' ') + cg.round(t);
-   let rect = model.add('square').move(0,1.5,0).scale(.2,.1,1);
+   let rect = model.add('square').move(0,0.5,0).scale(.2,.1,1);
    let beamL = new ControllerBeam(model, 'left');
    let beamR = new ControllerBeam(model, 'right');
 
@@ -23,7 +23,7 @@ export const init = async model => {
          rect.color(1,1,1);
          let u = uvdL[0], v = uvdL[1], d = uvdL[2];
          let text = 'u:' + round(u) + '\nv:' + round(v) + '\nd:' + round(d);
-         model.add(clay.text(text)).move(-.036,1.53,.001)
+         model.add(clay.text(text)).move(-.036,0.53,.001)
 	                           .scale(.02/inch).color(0,0,0);
 	 vibrate('left', u*u < .033 && v*v < .09 ? 1 : .5);
       }
@@ -34,8 +34,8 @@ export const init = async model => {
       let uvdR = beamR.hitRect(rect.getGlobalMatrix());
       if (uvdR) {
          rect.color(1,.5,.5);
-         let u = uvdL[0], v = uvdL[1];
-         model.add('diskZ').move(.2*u,1.5+.1*v,20.5,.001).scale(.01).color(0,0,0).dull();
+         let u = uvdR[0], v = uvdR[1];
+         model.add('diskZ').move(.2*u,0.5+.1*v,20.5,.001).scale(.01).color(0,0,0).dull();
 	 if (model.time % .04 < .02)
 	    vibrate('right', 1, 2);
       }
